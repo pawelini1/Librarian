fc2e920eabd75d725083923c73f1395ebe6601a2
Paweł Szymański <pawel.szymanski@grandparade.co.uk>
2019-01-08 23:36:18 +0100

    Initial Commit

---
+++ Librarian/Librarian.h
@@ -0,0 +1,19 @@
+//
+//  Librarian.h
+//  Librarian
+//
+//  Created by Pawel Szymanski on 08/01/2019.
+//  Copyright © 2019 Pawel Szymanski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Librarian.
+FOUNDATION_EXPORT double LibrarianVersionNumber;
+
+//! Project version string for Librarian.
+FOUNDATION_EXPORT const unsigned char LibrarianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Librarian/PublicHeader.h>
+
+
